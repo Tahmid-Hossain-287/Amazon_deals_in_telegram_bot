5794453ea6317f9b2d9e94c419fa7cb2de5aaefc
Tahmid-Hossain-287 <mstahmid0@gmail.com>
2022-01-27 16:37:26 +0600

    added cookies folder and deals file

--- amazon_scrape.py
+++ amazon_scrape.py
@@ -8,13 +8,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import traceback
+from time import sleep
 
 
 options = Options()
 # Specifying where the cookies will be stored.
-options.add_argument("--user-data-dir=C:\\Users\\Tahmid\\Programming\\automation\\cookies")
+options.add_argument("--user-data-dir=C:\\Users\\Tahmid\\Programming\\telegram_bot\\cookies")
 # Silences logs on terminal and keeps terminal looking clean.
-# os.environ['WDM_LOG_LEVEL'] = '0'
+os.environ['WDM_LOG_LEVEL'] = '0'
 # Saves driver on project directory.
 os.environ['WDM_LOCAL'] = '1'
 # Downloads and installs necessary driver version if already not done so.
@@ -26,3 +27,26 @@ driver.set_window_size(1260, 905) # Resizes the window to a specific size.
 driver.set_window_position(250, 70, windowHandle='current')
 
 print('Driver instantiated successfully')
+
+def launch_amazon_page():
+    # driver.get("https://www.amazon.it/")
+    driver.get("https://www.amazon.com/")
+
+    deals_page = WebDriverWait(driver, 4).until(
+        EC.presence_of_element_located(
+            # (By.XPATH, '//*[@id="nav-xshop"]/a[1]')
+            (By.CSS_SELECTOR, '#nav-xshop > a:nth-child(2)')
+        )
+    )
+    
+    print("deals page found")
+
+    deals_page.click()
+
+
+
+def main():
+    launch_amazon_page()
+
+if __name__ == "__main__":
+    main()
