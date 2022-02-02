df0f16e1d2454ae572a878ae712006d51d3c7aef
Tahmid-Hossain-287 <mstahmid0@gmail.com>
2022-02-02 12:37:31 +0600

    user friendly

---
+++ first_time_login.py
@@ -0,0 +1,36 @@
+import os, random
+from selenium import webdriver
+from selenium.webdriver.common import action_chains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from time import sleep
+
+options = Options()
+# Specifying where the cookies will be stored.
+options.add_argument("--user-data-dir=C:\\Users\\Tahmid\\Programming\\telegram_bot\\cookies")
+# Silences logs on terminal and keeps terminal looking clean.
+os.environ['WDM_LOG_LEVEL'] = '0'
+# Saves driver on project directory.
+os.environ['WDM_LOCAL'] = '1'
+# Downloads and installs necessary driver version if already not done so.
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+actions = action_chains.ActionChains(driver)
+
+# Setting window position and size.
+driver.set_window_size(1260, 905) # Resizes the window to a specific size.
+driver.set_window_position(250, 70, windowHandle='current')
+
+def launch_login_page():
+    driver.get(r'https://www.amazon.it/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.it%2Fyour-account%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=itflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&')
+    sleep(30)
+    driver.quit()
+
+launch_login_page()
+
+
