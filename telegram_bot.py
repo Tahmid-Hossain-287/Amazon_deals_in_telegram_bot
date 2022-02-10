425691696e3cd9d4a2bca4b905d0c60a33e0af10
Tahmid-Hossain-287 <mstahmid0@gmail.com>
2022-02-10 12:54:21 +0600

    updated invite link so that can add to new group

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,4 @@
-from product_link_scrape import launch_deals_page, driver, WebDriverWait, By, EC
+from product_link_scrape import launch_deals_page, driver, WebDriverWait, By, EC, options
 import telegram, re
 from secrets import token, group_id
 from time import sleep
@@ -7,6 +7,8 @@ from time import sleep
 bot = telegram.Bot(token=str(token))
 updates = bot.get_updates()
 
+def launch_page():
+    driver.get('https://www.amazon.it/')
 
 def get_product_information():
     try:
@@ -62,7 +64,7 @@ def get_product_information():
         return discount_amount, original_price, offer_price, saved_amount, image_link, product_title
         
     except Exception as e:
-        print(e)
+        # print(e)
         pass
 
 def send_telegram_message():
@@ -85,8 +87,11 @@ def send_telegram_message():
                 image_link = discount[4]
                 product_title = discount[5]
                 long_url = driver.current_url
+                group_invite = bot.create_chat_invite_link(chat_id=group_id).invite_link
+                polished_invite = "https://t.me/share/url?url=" + str(group_invite.replace("+", "%2B"))
+                print(polished_invite)
                 # template = f" 🔥🔥 *{discount_amount} OFF* ({image_link})🔥🔥\n 🤑 SUPER SCONTO 🤑\n 💣 *SOLO {offer_price}* ❌Invece di {original_price}\n 💰💲 Risparmi {saved_amount} 💰💲\n 👉 Apri su Amazon {line}\n {product_title}""
-                button_1 = telegram.InlineKeyboardButton(text='📩Invita un amico', url=r'https://t.me/share/url?url=https://t.me/amazoncouponpromo')
+                button_1 = telegram.InlineKeyboardButton(text='📩Invita un amico', url=polished_invite)
                 button_2 = telegram.InlineKeyboardButton(text="📱Apri nell'app", url=f'{long_url}')
                 keyboard_inline = telegram.InlineKeyboardMarkup([[button_1, button_2]])
                 bot.send_message(
@@ -96,7 +101,7 @@ def send_telegram_message():
                       reply_markup=keyboard_inline
                       )
             except Exception as e:
-                print(e)
+                # print(e)
                 continue    
     
     with open('deals.txt', 'w'):
@@ -105,6 +110,7 @@ def send_telegram_message():
     
 
 if __name__ == "__main__":
-    launch_deals_page('https://www.amazon.it/')
+    # launch_deals_page('https://www.amazon.it/')
+    launch_page()
     send_telegram_message()
 
