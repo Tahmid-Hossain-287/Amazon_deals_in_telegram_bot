a25cd92542c9cc7f00541e2868d1d44db7b99cc3
Tahmid Hossain <mstahmid0@gmail.com>
2022-03-30 22:16:38 +0600

    Added time to start and end bot.
    For some strange reason, the variables containing the bot to start and stop were not uploaded.

--- secrets.py
+++ secrets.py
@@ -1,3 +1,11 @@
-token = "token here" # As string
+token = "Token here" # As string
 group_id = group_id_here # As int including minus sign if any.   
+time_interval_for_each_message = 5 # In minutes
 
+link = "Enter link here"
+pages_to_retrieve = 5
+
+# Maintain the format used.
+time_to_collect_deals = "06:30:00"
+start_time = "09:30:00"
+end_time = 22
